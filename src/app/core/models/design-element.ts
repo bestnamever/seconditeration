33bd8e1fda943d70737843a9610b07186b94c0a0
Martin Peeters <406336@student.fontys.nl>
2021-10-20 10:40:10 +0200

    Added new Models for the design based on JSON structure

---
+++ src/app/core/models/design-element.ts
@@ -0,0 +1,11 @@
+import {WidgetType} from "./widget-type";
+import {AssetType} from "./asset-type";
+import {DesignElementvalue} from "./design-elementvalue";
+
+export interface DesignElement {
+  widgetType: WidgetType;
+  assetType: AssetType;
+  text: string;
+  values: Array<DesignElementvalue>;
+  details: Map<string, string>;
+}
