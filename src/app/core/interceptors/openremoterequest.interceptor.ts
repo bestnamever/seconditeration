aef3936c069c5447ae5a7f1c4b55a790001c87e3
u93639p88488 <leo@leonolle.nl>
2021-12-07 10:30:07 +0100

    connect with LEO'S backend fix

--- src/app/core/interceptors/openremoterequest.interceptor.ts
+++ src/app/core/interceptors/openremoterequest.interceptor.ts
@@ -32,10 +32,10 @@ export class OpenremoterequestInterceptor implements HttpInterceptor {
     request = request.clone({
       url: /*'http://martinaeytesting.nl:8080' +*/ request.url,
       headers: request.headers
-        .append("Authorization", auth)
-        .append("Access-Control-Allow-Origin", '*')
-        .append("Access-Control-Allow-Methods", 'POST, PUT, GET, OPTIONS, DELETE')
-        .append("Cache-Control", "no-cache")
+        // .append("Authorization", auth)
+        // .append("Access-Control-Allow-Origin", '*')
+        // .append("Access-Control-Allow-Methods", 'POST, PUT, GET, OPTIONS, DELETE')
+        // .append("Cache-Control", "no-cache")
        // .append("Access-Control-Expose-Headers", "*")
     });
     console.log("Going to execute the following request:")
