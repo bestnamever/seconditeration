ade96ad7bdba55b2af7a8474fa635a098fd5a0e3
yu hong <neverhasthisname@gmail.com>
2021-12-13 12:59:08 +0100

    changes for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  useLocalStorage:true
+  useLocalStorage:false,
+  useDatabase: true,
+  previewUrl: 'http://192.168.178.24:4200/preview',
+  backendUrl: 'http://207.180.246.34:8000',
+  openremoteUrl: 'http://martinaeytesting.nl:8080',
+  debug_components: false,
 };
