c3e96339f74e19e64ee8df0668fe71fc92fcb666
Bestnamever <yu.hong@dawnware.com>
2021-12-15 12:09:26 +0100

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  useLocalStorage:true,
+  useLocalStorage:false,
   useDatabase: true,
   previewUrl: 'https://bestnamever.github.io/seconditeration/preview',
-  backendUrl: 'http://207.180.246.34:8000',
+  backendUrl: 'https://backend.martinaey.nl',
   openremoteUrl: 'https://openremote.martinaey.nl',
   debug_components: false,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  useLocalStorage: true,
+  useLocalStorage: false,
   useDatabase: true,
   previewUrl: 'https://bestnamever.github.io/seconditeration/preview',
-  backendUrl: 'http://207.180.246.34:8000',
+  backendUrl: 'https://backend.martinaey.nl',
   openremoteUrl: 'https://openremote.martinaey.nl',
   debug_components: false,
 };
