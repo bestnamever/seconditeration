6a7409774275bf2c00375acf1fb8ba7e77b97561
Bestnamever <yu.hong@dawnware.com>
2021-12-15 10:44:54 +0100

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  useLocalStorage:true
+  useLocalStorage:true,
+  useDatabase: true,
+  previewUrl: 'https://bestnamever.github.io/seconditeration/preview',
+  backendUrl: 'http://207.180.246.34:8000',
+  openremoteUrl: 'https://openremote.martinaey.nl',
+  debug_components: false,
 };
