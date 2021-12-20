4595ff25a8e4d02b103c08d24c38c96c0fbcfcf4
Bestnamever <yu.hong@dawnware.com>
2021-12-20 15:04:26 +0100

    page text

--- src/app/core/services/preview.service.ts
+++ src/app/core/services/preview.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {WidgetComponent} from "../models/widget-component";
+import { BehaviorSubject, Observable } from "rxjs";
+import { WidgetComponent } from "../models/widget-component";
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,17 @@ export class PreviewService {
   private currentlySelectedWidgetSubject: BehaviorSubject<WidgetComponent | null>; // The state which we can edit
   public currentlySelectedWidgetState: Observable<WidgetComponent | null>; // The view-only state, where we can subscribe on to get updates.
 
+  private currentPageNameSubject: BehaviorSubject<string | null>;
+  public currentPageNameState: Observable<string | null>;
   // Constructor
   constructor() {
 
     // Initialize variables
     this.currentlySelectedWidgetSubject = new BehaviorSubject<WidgetComponent | null>(null); // Set the value on init
     this.currentlySelectedWidgetState = this.currentlySelectedWidgetSubject.asObservable(); // Make a clone of the state which is read-only
+
+    this.currentPageNameSubject = new BehaviorSubject<string | null>(null);
+    this.currentPageNameState = this.currentPageNameSubject.asObservable();
   }
 
   /* ---------------------------------------------- */
@@ -24,6 +29,10 @@ export class PreviewService {
   // Methods
   selectWidget(widget: WidgetComponent): void {
     this.currentlySelectedWidgetSubject.next(widget);
+  }
+
+  changePageName(name: string): void {
+    this.currentPageNameSubject.next(name);
   }
 
 
