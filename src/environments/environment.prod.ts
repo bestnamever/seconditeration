f3ab1a0272c1d0de781fad3ac34cdaf3097cb075
Bestnamever <yu.hong@dawnware.com>
2021-12-20 16:38:50 +0100

    development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
-  production: true,
-  useLocalStorage:true
+  production: false,
+  useLocalStorage: false,
+  useDatabase: true,
+  previewUrl: 'https://bestnamever.github.io/seconditeration/preview',
+  backendUrl: 'https://backend.martinaeytesting.nl',
+  openremoteUrl: 'https://openremote.martinaeytesting.nl',
+  debug_components: false,
 };
